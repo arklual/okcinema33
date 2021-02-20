c20d57e33a02632d4c27b3f5cd7404abdedaf578
Artem <66090455+siddot@users.noreply.github.com>
2021-02-20 15:29:09 +0300

    Update models.py

--- cinema/accounts/models.py
+++ cinema/accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+  user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
+  email = models.EmailField(null = True, blank = True)
+  
+  def __str__(self):
+    return self.user.username
+  
