2639aab82ce05d26251881949290d15885a1c394
Artyom Klushin <aklushin@localhost.localdomain>
2021-02-24 21:07:55 +0300

    5-fix bag in home

--- cinema/home/views.py
+++ cinema/home/views.py
@@ -1,17 +1,20 @@
 from django.shortcuts import render, get_object_or_404, Http404
 from films.models import Film
 from serials.models import Serial
+from itertools import chain
 
 def home(request):
     filmList = Film.objects.order_by('-date')
-    while filmList.count() > 3:
-        del filmList[filmList.count-1]
+    list1 = list(chain(filmList))
+    while len(list1) > 3:
+        del list1[len(list1)-1]
     serialList = Serial.objects.order_by('-date')
-    while serialList.count() > 3:
-        del serialList[serialList.count-1]
+    list2 = list(chain(serialList))
+    while len(list2) > 3:
+        del list2[len(list2)-1]
     context = {
-        "filmList": filmList,
-        "sList": serialList,
+        "filmList": list1,
+        "sList": list2,
         "title": "Главная страница кинотеатра",
     }
     return render(request, "partial/home.html", context)
