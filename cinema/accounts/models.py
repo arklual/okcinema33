983c0be3d3b966c232f0f600ea15032a783fb767
Artyom Klushin <aklushin@localhost.localdomain>
2021-04-02 22:18:11 +0300

    fix profile

--- cinema/accounts/models.py
+++ cinema/accounts/models.py
@@ -1,12 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 
 # Create your models here.
 class Profile(models.Model):
-  user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
+  user = models.OneToOneField(User, on_delete=models.CASCADE, null=False)
   email = models.EmailField(null = True, blank = True)
   image = models.ImageField()
 
   def __str__(self):
     return self.user.username
+  def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        profile, created = Profile.objects.get_or_create(user=instance)
+
+  post_save.connect(create_user_profile, sender=User)
