ef9f4620e415c96bbc3a5080742f50aa03b3ed35
Artyom Klushin <aklushin@localhost.localdomain>
2021-06-15 14:01:33 +0300

    fix heroku #2

--- cinema/settings.py
+++ cinema/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'pxp60z8tk)sbj7rveup^v_x8ve@gz39r#)2y164trfc*wvz*q4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -56,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'cinema.urls'
@@ -127,7 +128,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
@@ -135,6 +136,11 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
  
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media")
+
+
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 
 
